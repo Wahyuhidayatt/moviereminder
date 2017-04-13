70f651372c9b6e520ac3e79ddd002ee0c25fe031
hepiska <ego1403@gmail.com>
2017-04-13 10:06:04 +0700

    pull2

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
@@ -9,7 +11,7 @@ import 'element-ui/lib/theme-default/index.css'
 
 
 Vue.use(ElementUI)
-
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
